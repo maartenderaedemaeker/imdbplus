9d2418c70ec5d7ed058f2c49bcc5e601b426242b
Maarten De Raedemaeker <maarten.deraedemaeker@themasterlabs.com>
2022-03-31 15:48:33 +0200

    updated access code

--- utils/storyblok.js
+++ utils/storyblok.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import StoryblokClient from "storyblok-js-client";
 
 const Storyblok = new StoryblokClient({
-  accessToken: "c1g4yy5Pro9MeJrxV9grQAtt",
+  accessToken: "eVG9JErb5J9WEZ7kAfHxvAtt",
   cache: {
     clear: "auto",
     type: "memory",
